f59560514117ef5b813f8eb7c389a95073946ead
adham618 <adhamtarek291gmail.com>
2021-11-12 23:36:23 +0200

    complete JS

---
+++ main.js
@@ -0,0 +1,19 @@
+const about = document.querySelector(".about");
+const btns = document.querySelectorAll(".tab-btn");
+const contents = document.querySelectorAll(".content");
+about.addEventListener("click", function (e) {
+  const id = e.target.dataset.id;
+  if (id) {
+    // remove selected from other buttons
+    btns.forEach(function (btn) {
+      btn.classList.remove("active");
+    });
+    e.target.classList.add("active");
+    // hide other articles
+    contents.forEach(function (content) {
+      content.classList.remove("active");
+    });
+    const element = document.getElementById(id);
+    element.classList.add("active");
+  }
+});
